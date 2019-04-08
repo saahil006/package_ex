5000fb9d399cf6798316e967a4ef2fbea178f4c8
saahil006 <40289009+saahil006@users.noreply.github.com>
2019-04-08 12:16:06 +0530

    Add files via upload

---
+++ newpkl.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 11:02:38 2019
+
+@author: Saha
+"""
+
+import pickle
+
+mydata = pickle.load (file=open("C:/Users/Saha/Downloads/infersent.allnli.pickle","rb"))
+mydata
